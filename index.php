52ec07e1384e4cc04280a9dbdee4c49cb6993b33
cyptrid <cyptrid@gmail.com>
2021-01-27 18:56:12 +0700

    Class dan Object

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+// Cara membuat Class & Object
+
+
+// buat class
+class Coba {
+
+}
+
+// buat object
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
